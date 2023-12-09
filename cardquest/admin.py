dd3860f00231b16d9bb71aedbc3509de4994f818
Ohn08 <202280234@psu.palawan.edu.ph>
2023-12-09 12:09:20 +0800

    fix bg

--- cardquest/admin.py
+++ cardquest/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from .models import Trainer, PokemonCard
+from .models import Trainer, PokemonCard, Collection
 
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
     list_display = ('name', 'birthdate', 'location', 'email')
-
     search_fields = ('name', 'location')
 
 @admin.register(PokemonCard)
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ('name', 'rarity')
     search_fields = ('name',)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('trainer', 'card', 'collection_date')
+    search_fields = ('trainer__name', 'card__name')
