643a83489b326ae12669cdc99d148024e6cbdfea
Ohn08 <202280234@psu.palawan.edu.ph>
2023-12-13 11:22:11 +0800

    add intitial data

--- cardquest/admin.py
+++ cardquest/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
-from .models import Trainer, PokemonCard, Collection
+from . import models
 
-@admin.register(Trainer)
+
+@admin.register(models.Trainer)
 class TrainerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'birthdate', 'location', 'email')
-    search_fields = ('name', 'location')
+    list_display = ("name", "birthdate", "location", "email", "created_at", "updated_at")
+    search_fields = ("name", "location",)
 
-@admin.register(PokemonCard)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rarity')
-    search_fields = ('name',)
+@admin.register(models.PokemonCard)
+class PokemonCardAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities", "created_at", "updated_at")
+    search_fields = ("name",)
 
-@admin.register(Collection)
+@admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('trainer', 'card', 'collection_date')
-    search_fields = ('trainer__name', 'card__name')
+    list_display = ("card", "trainer", "collection_date", "created_at", "updated_at")
+    search_fields = ("card", "trainer")
